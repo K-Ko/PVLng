4fc4ef750efe04eb3a1be98d53d4f114c6662a2a
patrick.feisthammel@citrin.ch <patrick.feisthammel@citrin.ch>
2014-04-20 14:01:23 +0200

    Changed Event Counter to model Switcher, because the event counter is delivered in every case, non only on changes

--- sql/pvlng.feature_teslamotors.sql
+++ sql/pvlng.feature_teslamotors.sql
@@ -5,7 +5,7 @@ INSERT INTO `pvlng_type` (`id`, `name`, `description`, `model`, `unit`, `childs`
   VALUES ('69', 'Percent', 'Percent Value', 'Sensor', '%', '0', '1', '1', '1', '/images/ico/edit_percent.png'); 
 
 INSERT INTO `pvlng_type` (`id`, `name`, `description`, `model`, `unit`, `childs`, `read`, `write`, `graph`, `icon`) \
-  VALUES ('92', 'Event Counter', 'Counter', 'Sensor', '', '0', '1', '1', '1', '/images/ico/wheel.png');
+  VALUES ('92', 'Event Counter', 'Counter', 'Switcher', '', '0', '1', '1', '1', '/images/ico/wheel.png');
 
 INSERT INTO `pvlng_type` (`id`, `name`, `description`, `model`, `unit`, `childs`, `read`, `write`, `graph`, `icon`) \
   VALUES ('93', 'Distance', 'Distance', 'Sensor', '', '0', '1', '1', '1', '/images/ico/wheel.png');
